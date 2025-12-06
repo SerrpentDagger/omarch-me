34ae5857bb81bbe48790ae11d8554bca98867aa1
SerrpentDagger <serpentdagger.buisness@gmail.com>
2025-12-06 15:41:57 +0100

    local only inside functions

--- install/post-install/prune-desktops.sh
+++ install/post-install/prune-desktops.sh
@@ -1,6 +1,6 @@
 echo 'Removing orphaned .desktop files...'
 
-local applications_path="$HOME/.local/share/applications"
+applications_path="$HOME/.local/share/applications"
 
 mpv --version &>/dev/null || rm -f "$applications_path/mpv.desktop"
 pacman -Q typora &>/dev/null || rm -f "$applications_path/typora.desktop"
